589cb7be924fb38118a1afc3af54ff69607fb331
rayaherrera <rayaherrera@aol.com>
2019-09-30 09:01:38 -0500

    Learned how to do maths

--- learning.py
+++ learning.py
@@ -1,4 +1,13 @@
+# this is an octothrope -- it makes a comment (the line is ignored by the computer)
 print("hello world")
 print("Howdy' y'all")
 print("I like typing this.")
 print("This is fun.")
+
+# math skills demo
+print("I will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now I will count the eggs", 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 < 5 - 7)
