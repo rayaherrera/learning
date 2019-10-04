1a42da07da470e4e3ed99d744ef67297f8cab060
rayaherrera <rayaherrera@aol.com>
2019-10-04 08:23:43 -0500

    Learned how to do maths

--- learning.py
+++ learning.py
@@ -1,13 +1,23 @@
 # this is an octothrope -- it makes a comment (the line is ignored by the computer)
+# this prints the text in green
 print("hello world")
+# this prints the text in green
 print("Howdy' y'all")
+# this prints the text in green
 print("I like typing this.")
+# this prints the text in green
 print("This is fun.")
 
 # math skills demo
-print("I will not count my chickens", 2)
+# this displays the text, but also displays the number 2
+print("I will now count my chickens", 2)
+# this line completes a mathematical function
 print("Hens: ", 25+30/6)
+# this line calculates the function inside the parenthesis
 print("Roosters: ", 100 - 25 * 3 % 4)
+# this line prints the text and then displays line 3
 print("Now I will count the eggs", 7)
+# this line calculates the function inside the parenthesis
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# this line displays that the statement in the parenthesis is false
 print(3 + 2 < 5 - 7)
