3dde1479edef17b6a46e60ce13d036383e15a579
rayaherrera <rayaherrera@aol.com>
2019-10-29 08:57:55 -0500

    I went back and made sure that I had practiced playing with variables

--- learning.py
+++ learning.py
@@ -1,3 +1,4 @@
+# September 30
 # this is an octothrope -- it makes a comment (the line is ignored by the computer)
 # this prints the text in green
 print("hello world")
@@ -21,3 +22,47 @@ print("Now I will count the eggs", 7)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 # this line displays that the statement in the parenthesis is false
 print(3 + 2 < 5 - 7)
+print("I will now count the cows")
+print(35.0 - 25.0 + 10.0 - 15.0)
+
+# October 3
+# Variables and some of their powers
+cars = 100
+space_in_car = 4.0
+drivers = 45
+passengers = 115
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = space_in_car * cars_driven
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars available.")
+print("there are", drivers, "drivers available today.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put approximately", average_passengers_per_car, "in each car.")
+
+# a story using variables
+teams = 10
+number_of_games = 100
+games_per_team = number_of_games / teams
+player_to_a_team = 9
+players = 120
+players_that_can_play = teams * player_to_a_team
+players_who_cannot_play = players - players_that_can_play
+
+
+print("There are", teams, "teams available.")
+print("There will be", players_who_cannot_play, "who will not make a team.")
+print("There will be", players_that_can_play, "as a whole.")
+print("")
+
+# More variables and playing with output
+myName = "Ray"
+myAge = "18"
+myColor = "pink"
+
+print("Let's talk about %s." % myName)
+print("His favorite color is %s. " % (myColor))
+print("And he's %s years old!." % (myAge))
